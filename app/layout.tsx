6108eae88b81a73c4978eb2fd56699ff777c86cd
Ihor222 <miskivigor10@gmail.com>
2025-08-22 00:46:46 +0300

    lastDanc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,19 @@ import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+type LayoutProps = {
+  children: React.ReactNode;
+  modal?: React.ReactNode; 
+};
+
+export default function RootLayout({ children, modal }: LayoutProps) {
   return (
     <html lang="en">
       <body>
         <TanStackProvider>
           <Header />
           <main>{children}</main>
+          {modal}
           <Footer />
         </TanStackProvider>
       </body>
