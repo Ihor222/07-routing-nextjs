8d500f114ae3031b18a6d3369efc64930c6bb9e2
Ihor222 <miskivigor10@gmail.com>
2025-08-22 14:26:52 +0300

    ghfjkd

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,12 @@
-import css from "./home.modal.css"
+import styles from "./NotFound.module.css";
 
-const NotFound = () => {
-    return (
-        <>
-            <h1 className={css.title}>404 - Page not found</h1>
-            <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
-        </>
-    );
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 - Page not found</h1>
+      <p className={styles.description}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
 }
-export default NotFound;
